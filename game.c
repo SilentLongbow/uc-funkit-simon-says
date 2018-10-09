d8036b184756f1e1acc633bdbcf23288000671c2
Siyuan Wei <swe50@cs17090jp.canterbury.ac.nz>
2018-10-09 21:03:09 +1300

    add some useful initialise for game and add a function to decide which player start first

--- game.c
+++ game.c
@@ -1,14 +1,97 @@
-#include "system.h"
+/* Author: Siyuan WEI(ID:22883373)
+ *         Matthew Kenny(ID:)
+ * 
+ * describle: main file that run the game
+ */
 
+
+
+#include "system.h"
+#include "navswitch.h"
+#include "pacer.h"
+#include "tinygl.h"
+
+
+
+
+
+
+#define PACER_RATE 500  
+
+
+
+
+
+
+
+
+
+
+
+
+/* init drivers */
+void system_initialise(void)
+{
+    // general system init
+    system_init();
+    
+    // naveswitch init
+    navswitch_init();
+    
+    //timer init
+    pacer_init(PACER_RATE);
+    
+    
+    //IR init
+    ir_uart_init();
+    
+    
+    //LED matrix init
+    //trying to figure it out
+    
+    
+    
+    //LED1 init(might be useful)
+    led_init ();
+    led_set(LED1, 0);
+    
+    
+}
+
+
+
+/*dicide which player send message first*/
+int decide_first_player(void)
+{
+    
+    
+    
+    
+    
+    
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+// play the game
 int main (void)
 {
-    system_init ();
+    
 
 
     while (1)
     {
-
-
 
     }
 }
