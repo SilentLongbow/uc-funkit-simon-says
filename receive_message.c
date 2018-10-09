ac5f6eeb8cfa2df4d454d127fccf6a3f0b765ddb
Siyuan Wei <swe50@cs17090jp.canterbury.ac.nz>
2018-10-09 21:14:44 +1300

    same like send_message

---
+++ receive_message.c
@@ -0,0 +1,14 @@
+/* Author: Siyuan WEI(ID:22883373)
+ *         Matthew Kenny(ID:)
+ * 
+ * describle: receive message calling by game.c
+ */
+
+
+
+#include <stdio.h>
+
+char receive_message()
+{
+    
+}
