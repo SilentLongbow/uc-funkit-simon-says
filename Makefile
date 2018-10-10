f42c09ddc6223a1f51c662f8a9eb734c86ba2a13
mjk141 <mjk141@uclive.ac.nz>
2018-10-10 16:27:10 +1300

    Linked all dependencies in Makefile and called them in game

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # File:   Makefile
-# Author: Matthew Kenny
+# Author: Matthew Kenny: 11031880
 # Date:   10 Oct 2018
 # Descr:  Makefile for game
 
@@ -34,12 +34,38 @@ pacer.o: ../../utils/pacer.c ../../drivers/avr/system.h ../../drivers/avr/timer.
 led.o: ../../drivers/led.c ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/led.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
+navswitch.o: ../../drivers/navswitch.c ../../drivers/avr/system.h ../../drivers/navswitch.h ../../drivers/avr/pio.h ../../drivers/avr/delay.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+ir_uart.o: ../../drivers/avr/ir_uart.c ../../drivers/avr/ir_uart.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+tinygl.o: ../../utils/tinygl.c ../../drivers/avr/system.h ../../drivers/display.h ../../utils/font.h ../../utils/tinygl.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+timer0.o: ../../drivers/avr/timer0.c ../../drivers/avr/bits.h ../../drivers/avr/prescale.h ../../drivers/avr/system.h ../../drivers/avr/timer0.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+usart1.o: ../../drivers/avr/usart1.c ../../drivers/avr/system.h ../../drivers/avr/usart1.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+display.o: ../../drivers/display.c ../../drivers/avr/system.h ../../drivers/display.h ../../drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+ledmat.o: ../../drivers/ledmat.c ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/ledmat.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+prescale.o: ../../drivers/avr/prescale.c ../../drivers/avr/prescale.h ../../drivers/avr/system.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
+font.o: ../../utils/font.c ../../drivers/avr/system.h ../../utils/font.h
+	$(CC) -c $(CFLAGS) $< -o $@
 
 
 
 
 # Link: create ELF output file from object files.
-game.out: game.o pio.o system.o timer.o pacer.o led.o
+game.out: game.o pio.o system.o timer.o pacer.o led.o navswitch.o ir_uart.o tinygl.o timer0.o usart1.o display.o ledmat.o prescale.o font.o
 	$(CC) $(CFLAGS) $^ -o $@ -lm
 	$(SIZE) $@
 
--- game.c
+++ game.c
@@ -1,11 +1,6 @@
-/* Author: Siyuan WEI(ID:22883373)
- *         Matthew Kenny(ID:)
-<<<<<<< HEAD
- *
-=======
- *
->>>>>>> ac5f6eeb8cfa2df4d454d127fccf6a3f0b765ddb
- * describle: main file that run the game
+/* Author: Siyuan WEI(ID: 022883373)
+ *         Matthew Kenny(ID: 11031880)
+ * Description: The main game file
  */
 
 
@@ -15,47 +10,83 @@
 #include "pacer.h"
 #include "led.h"
 #include "navswitch.h"
-#include "pacer.h"
 #include "tinygl.h"
+#include "ir_uart.h"
 
 #define PACER_RATE 500
 
-/* init drivers */
+/** Define PIO pins driving LED matrix rows.  */
+static const pio_t rows[] = {
+    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
+    LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
+    LEDMAT_ROW7_PIO
+};
+
+
+/** Define PIO pins driving LED matrix columns.  */
+static const pio_t cols[] = {
+    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
+    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
+};
+
+/** Initialise all of the required drivers */
 void system_initialise(void)
 {
-    // general system init
+    // general system initialiser
     system_init();
 
-    // naveswitch init
+    // navigational switch initialiser
     navswitch_init();
 
-    //timer init
+    //timer initialiser
     pacer_init(PACER_RATE);
 
 
-    //IR init
+    //IR initialiser
     ir_uart_init();
 
 
-    //LED matrix init
-    //trying to figure it out
+    //LED matrix initialiser
+    int i;
+    for (i = 0; i < 7; i++) {
+        pio_config_set(rows[i], PIO_OUTPUT_HIGH);
+    }
+    for (i = 0; i < 5; i++) {
+        pio_config_set(cols[i], PIO_OUTPUT_HIGH);
+    }
 
 
-    //LED1 init(might be useful)
+    //LED initialiser (might be useful)
     led_init ();
-    led_set(LED1, 0);
+
 
 
 }
 
 /*dicide which player send message first*/
+/*
 int decide_first_player(void)
+{
+}
+* */
+
+/** Turns on the blue LED when called */
+void blue_led_on()
+{
+    led_set(LED1, 1);
+}
+
+/** Turns off the blue LED when called */
+void  blue_led_off()
+{
+    led_set(LED1, 0);
+}
 
 // play the game
 int main (void)
 {
-    system_init ();
-    led_init ();
+    system_initialise();    // Turn on the blue LED
+    blue_led_on();
     while (1) {
     }
 }
