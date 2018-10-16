374e2282f52a3982a69569b39f9332331a41b339
Siyuan Wei <swe50@cs18198kq.canterbury.ac.nz>
2018-10-16 15:43:08 +1300

    add header file of receive_message mode

---
+++ receive_message.h
@@ -0,0 +1,18 @@
+/**  @file   displayMessage.h
+     @author Siyuan Wei
+     @ header file of receive_message
+*/
+
+
+#ifndef RECEIVE_MESSAGE_H
+#define RECEIVE_MESSAGE_H
+
+
+#include "system.h"
+
+
+
+char receive_message(int message_length);
+
+
+#endif
