25c507c6f78f13b5270a6ab81fbccbc20d2126d4
Siyuan Wei <swe50@cs18198kq.canterbury.ac.nz>
2018-10-16 15:54:31 +1300

    add header file of nav_motion.c

---
+++ nav_motion.h
@@ -0,0 +1,20 @@
+/**  @file   displayMessage.h
+     @author Siyuan Wei
+     @ header file of nav_motion.c
+*/
+
+
+#ifndef NAV_MOTION_H
+#define NAV_MOTION_H
+
+#include "system.h"
+#include "navswitch.h"
+
+
+// return N,E,S,W corresponding direction recorded by nav_motion
+char nav_motion(void);
+
+
+#endif
+
+
